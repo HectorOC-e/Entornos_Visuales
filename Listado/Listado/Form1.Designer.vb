cf86c72a27eef97c50f8f3afcbd8891ce4377e62
Hector Castellanos <osoriohector89@gmail.com>
2020-05-26 08:23:09 -0600

    Nuevo ejercicio, Listado

---
+++ Listado/Listado/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cbbLista = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cbbLista
+        '
+        Me.cbbLista.FormattingEnabled = True
+        Me.cbbLista.Items.AddRange(New Object() {"Café", "Refresco", "Té", "Agua"})
+        Me.cbbLista.Location = New System.Drawing.Point(115, 48)
+        Me.cbbLista.Name = "cbbLista"
+        Me.cbbLista.Size = New System.Drawing.Size(121, 24)
+        Me.cbbLista.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(136, 144)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Mostrar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(362, 234)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cbbLista)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cbbLista As ComboBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Listado/Listado/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim itemId As Integer
+        Dim itemNombre As String
+
+        itemId = cbbLista.SelectedIndex
+        itemNombre = cbbLista.SelectedItem.ToString
+
+        Select Case itemId
+            Case 0
+                MsgBox(itemNombre)
+            Case 1
+                MsgBox(itemNombre)
+            Case 2
+                MsgBox(itemNombre)
+            Case 3
+                MsgBox(itemNombre)
+            Case Else
+                MsgBox("Seleccione un valor correcto")
+
+        End Select
+
+    End Sub
+
+End Class
