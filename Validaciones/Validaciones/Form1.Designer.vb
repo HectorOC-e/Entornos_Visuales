ce343fe383c5308b39bbfa66b57a30abfd791c02
Hector Castellanos <osoriohector89@gmail.com>
2020-05-30 15:39:25 -0600

    Validacion y boton limpiar

--- Validaciones/Validaciones/Form1.Designer.vb
+++ Validaciones/Validaciones/Form1.Designer.vb
@@ -22,29 +22,260 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.txtValor2 = New System.Windows.Forms.TextBox()
+        Me.txtValor1 = New System.Windows.Forms.TextBox()
+        Me.txtSuma = New System.Windows.Forms.TextBox()
+        Me.txtMultiplicacion = New System.Windows.Forms.TextBox()
+        Me.txtResta = New System.Windows.Forms.TextBox()
+        Me.txtDivision = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
         Me.btnValidar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(225, 173)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 0
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(55, 173)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 1
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'txtValor2
+        '
+        Me.txtValor2.Location = New System.Drawing.Point(133, 108)
+        Me.txtValor2.Name = "txtValor2"
+        Me.txtValor2.Size = New System.Drawing.Size(100, 22)
+        Me.txtValor2.TabIndex = 2
+        '
+        'txtValor1
+        '
+        Me.txtValor1.Location = New System.Drawing.Point(133, 51)
+        Me.txtValor1.Name = "txtValor1"
+        Me.txtValor1.Size = New System.Drawing.Size(100, 22)
+        Me.txtValor1.TabIndex = 2
+        '
+        'txtSuma
+        '
+        Me.txtSuma.Location = New System.Drawing.Point(55, 243)
+        Me.txtSuma.Name = "txtSuma"
+        Me.txtSuma.Size = New System.Drawing.Size(100, 22)
+        Me.txtSuma.TabIndex = 3
+        '
+        'txtMultiplicacion
+        '
+        Me.txtMultiplicacion.Location = New System.Drawing.Point(55, 320)
+        Me.txtMultiplicacion.Name = "txtMultiplicacion"
+        Me.txtMultiplicacion.Size = New System.Drawing.Size(100, 22)
+        Me.txtMultiplicacion.TabIndex = 4
+        '
+        'txtResta
+        '
+        Me.txtResta.Location = New System.Drawing.Point(200, 243)
+        Me.txtResta.Name = "txtResta"
+        Me.txtResta.Size = New System.Drawing.Size(100, 22)
+        Me.txtResta.TabIndex = 5
+        '
+        'txtDivision
+        '
+        Me.txtDivision.Location = New System.Drawing.Point(200, 320)
+        Me.txtDivision.Name = "txtDivision"
+        Me.txtDivision.Size = New System.Drawing.Size(100, 22)
+        Me.txtDivision.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(52, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 17)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Valor 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(52, 113)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 17)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Valor 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(52, 223)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 17)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Suma"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(197, 223)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 17)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Resta"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(52, 300)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 17)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Multiplicacion"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(197, 300)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(57, 17)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Division"
+        '
         'btnValidar
         '
-        Me.btnValidar.Location = New System.Drawing.Point(601, 109)
+        Me.btnValidar.Location = New System.Drawing.Point(139, 173)
         Me.btnValidar.Name = "btnValidar"
         Me.btnValidar.Size = New System.Drawing.Size(75, 23)
-        Me.btnValidar.TabIndex = 0
-        Me.btnValidar.Text = "Validar"
+        Me.btnValidar.TabIndex = 13
+        Me.btnValidar.Text = "VALIDAR"
         Me.btnValidar.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(356, 383)
         Me.Controls.Add(Me.btnValidar)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDivision)
+        Me.Controls.Add(Me.txtResta)
+        Me.Controls.Add(Me.txtMultiplicacion)
+        Me.Controls.Add(Me.txtSuma)
+        Me.Controls.Add(Me.txtValor1)
+        Me.Controls.Add(Me.txtValor2)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.btnLimpiar)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents txtValor2 As TextBox
+    Friend WithEvents txtValor1 As TextBox
+    Friend WithEvents txtSuma As TextBox
+    Friend WithEvents txtMultiplicacion As TextBox
+    Friend WithEvents txtResta As TextBox
+    Friend WithEvents txtDivision As TextBox
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
     Friend WithEvents btnValidar As Button
+
+    Private Sub btnValidar_Click(sender As Object, e As EventArgs) Handles btnValidar.Click
+        Dim valor1 As Int16
+        Dim valor2 As Int16
+        If txtValor1.Text = "" Then
+            MsgBox("Escriba un numero en la casilla", vbInformation)
+        ElseIf Not IsNumeric(txtValor1.Text) Then
+            MsgBox("Ingrese un valor numerico", vbInformation)
+            txtValor1.Text = ""
+        ElseIf IsNumeric(valor1) Then
+            valor1 = Val(txtValor1.Text)
+            If valor1 = 0 Then
+                MsgBox("Numero cero", vbInformation)
+            ElseIf valor1 > 0 Then
+                MsgBox("Valor Positivo")
+            ElseIf valor1 < 0 Then
+                MsgBox("Valor Negativo")
+            End If
+        End If
+
+        If txtValor2.Text = "" Then
+            MsgBox("Escriba un numero en la casilla", vbInformation)
+        ElseIf Not IsNumeric(txtValor1.Text) Then
+            MsgBox("Ingrese un valor numerico", vbInformation)
+            txtValor1.Text = ""
+        ElseIf IsNumeric(valor2) Then
+            valor2 = Val(txtValor1.Text)
+            If valor2 = 0 Then
+                MsgBox("Numero cero", vbInformation)
+            ElseIf valor2 > 0 Then
+                MsgBox("Valor Positivo")
+            ElseIf valor2 < 0 Then
+                MsgBox("Valor Negativo")
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim valor1 As Integer
+        Dim valor2 As Integer
+        Dim suma As Integer
+        Dim resta As Integer
+        Dim multiplicacion As Integer
+        Dim division As Double
+
+        valor1 = Val(txtValor1.Text)
+        valor2 = Val(txtValor2.Text)
+
+        suma = valor1 + valor2
+        resta = valor1 - valor2
+        multiplicacion = valor1 * valor2
+        division = valor1 / valor2
+
+        txtSuma.Text = suma
+        txtResta.Text = resta
+        txtMultiplicacion.Text = multiplicacion
+
+        If valor2 = 0 Then
+            MsgBox("No se puede dividir entre 0")
+        ElseIf valor2 > 0 Then
+            txtDivision.Text = division
+        End If
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        txtValor2.Clear()
+        txtValor1.Clear()
+        txtSuma.Clear()
+        txtResta.Clear()
+        txtMultiplicacion.Clear()
+        txtDivision.Clear()
+    End Sub
 End Class
