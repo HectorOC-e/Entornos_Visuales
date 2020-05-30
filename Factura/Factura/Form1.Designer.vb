28ad27859f112625390e1c1f1955443e7a8bcf36
Hector Castellanos <osoriohector89@gmail.com>
2020-05-30 13:32:41 -0600

    Factura sencilla

---
+++ Factura/Factura/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.txtPUnitario = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtSubtotal = New System.Windows.Forms.TextBox()
+        Me.txtDescuento = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.chkDescuento = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.chkDescuento)
+        Me.GroupBox1.Controls.Add(Me.txtCantidad)
+        Me.GroupBox1.Controls.Add(Me.txtPUnitario)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(19, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(269, 184)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos requeridos"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(118, 85)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(83, 22)
+        Me.txtCantidad.TabIndex = 6
+        '
+        'txtPUnitario
+        '
+        Me.txtPUnitario.Location = New System.Drawing.Point(118, 39)
+        Me.txtPUnitario.Name = "txtPUnitario"
+        Me.txtPUnitario.Size = New System.Drawing.Size(83, 22)
+        Me.txtPUnitario.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Cantidad"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "P. Unitario"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtTotal)
+        Me.GroupBox2.Controls.Add(Me.txtSubtotal)
+        Me.GroupBox2.Controls.Add(Me.txtDescuento)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(22, 256)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(266, 222)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Resultado"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Enabled = False
+        Me.txtTotal.Location = New System.Drawing.Point(115, 131)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(83, 22)
+        Me.txtTotal.TabIndex = 9
+        '
+        'txtSubtotal
+        '
+        Me.txtSubtotal.Enabled = False
+        Me.txtSubtotal.Location = New System.Drawing.Point(115, 83)
+        Me.txtSubtotal.Name = "txtSubtotal"
+        Me.txtSubtotal.Size = New System.Drawing.Size(83, 22)
+        Me.txtSubtotal.TabIndex = 8
+        '
+        'txtDescuento
+        '
+        Me.txtDescuento.Enabled = False
+        Me.txtDescuento.Location = New System.Drawing.Point(115, 37)
+        Me.txtDescuento.Name = "txtDescuento"
+        Me.txtDescuento.Size = New System.Drawing.Size(83, 22)
+        Me.txtDescuento.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(14, 131)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Total"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(14, 83)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Subtotal"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 40)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Descuento"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(118, 202)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(88, 38)
+        Me.btnCalcular.TabIndex = 2
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'chkDescuento
+        '
+        Me.chkDescuento.AutoSize = True
+        Me.chkDescuento.Location = New System.Drawing.Point(20, 132)
+        Me.chkDescuento.Name = "chkDescuento"
+        Me.chkDescuento.Size = New System.Drawing.Size(98, 21)
+        Me.chkDescuento.TabIndex = 7
+        Me.chkDescuento.Text = "Descuento"
+        Me.chkDescuento.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(347, 490)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents txtPUnitario As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents txtSubtotal As TextBox
+    Friend WithEvents txtDescuento As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents chkDescuento As CheckBox
+End Class
---
+++ Factura/Factura/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim pUnitario, cantidad As Integer
+        Dim descuento, subtotal, total As Double
+        Dim aplicaDescuento As Boolean
+
+        pUnitario = Val(txtPUnitario.Text)
+        cantidad = Val(txtCantidad.Text)
+        aplicaDescuento = chkDescuento.Checked
+        descuento = 0
+        subtotal = pUnitario * cantidad
+        If aplicaDescuento = True Then
+            descuento = subtotal * 0.5
+        End If
+        total = subtotal - descuento
+
+        txtSubtotal.Text = subtotal
+        txtDescuento.Text = descuento
+        txtTotal.Text = total
+    End Sub
+
+End Class
