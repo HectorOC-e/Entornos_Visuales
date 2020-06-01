0f8abaf349183c59cb1e476102628e9605d73510
Hector Castellanos <osoriohector89@gmail.com>
2020-06-01 17:11:47 -0600

    Acumulador Contador

---
+++ AcumuladorContador/AcumuladorContador/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPedido = New System.Windows.Forms.MaskedTextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtIndividual = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnsolicitar = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Orden Combos"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(11, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Total a Pagar"
+        '
+        'txtPedido
+        '
+        Me.txtPedido.Location = New System.Drawing.Point(141, 36)
+        Me.txtPedido.Mask = "99"
+        Me.txtPedido.Name = "txtPedido"
+        Me.txtPedido.Size = New System.Drawing.Size(100, 22)
+        Me.txtPedido.TabIndex = 2
+        Me.txtPedido.ValidatingType = GetType(Integer)
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtPedido)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.GroupBox1.Location = New System.Drawing.Point(23, 32)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(279, 122)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Pedidos"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.txtIndividual)
+        Me.GroupBox2.Controls.Add(Me.txtTotal)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Location = New System.Drawing.Point(23, 176)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(279, 129)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Pago"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(141, 36)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(100, 22)
+        Me.txtTotal.TabIndex = 5
+        '
+        'txtIndividual
+        '
+        Me.txtIndividual.Location = New System.Drawing.Point(141, 91)
+        Me.txtIndividual.Name = "txtIndividual"
+        Me.txtIndividual.Size = New System.Drawing.Size(100, 22)
+        Me.txtIndividual.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(11, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Individual"
+        '
+        'btnsolicitar
+        '
+        Me.btnsolicitar.Location = New System.Drawing.Point(324, 68)
+        Me.btnsolicitar.Name = "btnsolicitar"
+        Me.btnsolicitar.Size = New System.Drawing.Size(81, 61)
+        Me.btnsolicitar.TabIndex = 5
+        Me.btnsolicitar.Text = "Solicitar Orden"
+        Me.btnsolicitar.UseVisualStyleBackColor = True
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(324, 154)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(81, 30)
+        Me.btnNuevo.TabIndex = 6
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(433, 370)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.btnsolicitar)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "AcumuladorContador"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPedido As MaskedTextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtIndividual As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents btnsolicitar As Button
+    Friend WithEvents btnNuevo As Button
+End Class
---
+++ AcumuladorContador/AcumuladorContador/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub MaskedTextBox1_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles txtPedido.MaskInputRejected
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub btnsolicitar_Click(sender As Object, e As EventArgs) Handles btnsolicitar.Click
+        Try
+            Dim precio, total, totalPagar, orden As Integer
+
+            orden = txtPedido.Text
+
+            For i = 1 To orden Step 1
+                precio = InputBox("Ingresa el precio del platillo No." & i, "Precio del platillo")
+                totalPagar += precio
+
+            Next
+
+            total = totalPagar / orden
+            txtTotal.Text = totalPagar
+            txtIndividual.Text = total
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        txtPedido.Clear()
+        txtTotal.Clear()
+        txtIndividual.Clear()
+    End Sub
+End Class
